160fa78577790d1a0eef08af9ac1df60411a39e4
Bacchus Jackson <34331871+BacchusJackson@users.noreply.github.com>
2024-03-29 13:37:28 -0500

    fix: entrypoint syntax bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,7 @@ else
   echo $DOCKER_AUTH_JSON | jq . > ~/.docker/config.json
 fi
 
-if [[ -z $WFE_IMAGE_BUILD_ENABLED && -n $WFE_IMAGE_SCAN_ENABLED ]]; then
+if [[ -z "$WFE_IMAGE_BUILD_ENABLED" && -n "$WFE_IMAGE_SCAN_ENABLED" ]]; then
   echo "Pull Image Scan target tag. Image Build not enabled, Image Scan enabled."
   docker pull "$WFE_IMAGE_TAG"
 fi
