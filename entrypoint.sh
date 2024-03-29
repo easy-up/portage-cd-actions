d668e964fbbce3eeb858610cc815e55c396d9090
Bacchus Jackson <34331871+BacchusJackson@users.noreply.github.com>
2024-03-29 12:49:09 -0500

    fix: add image pull if image build is disabled

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,10 @@ else
   echo $DOCKER_AUTH_JSON | jq . > ~/.docker/config.json
 fi
 
+if [[ -z $WFE_IMAGE_BUILD_ENABLED && -n $WFE_IMAGE_SCAN_ENABLED ]]; then
+  docker pull "$WFE_IMAGE_TAG"
+fi
+
 git config --global --add safe.directory $GITHUB_WORKSPACE
 
 workflow-engine run all --verbose --semgrep-experimental
