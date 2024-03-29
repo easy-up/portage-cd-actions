d1b8880000b40c07461379c4ec11a0f7512228cf
Bacchus Jackson <34331871+BacchusJackson@users.noreply.github.com>
2024-03-29 13:32:42 -0500

    chore: add debug in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,7 @@ else
 fi
 
 if [[ -z $WFE_IMAGE_BUILD_ENABLED && -n $WFE_IMAGE_SCAN_ENABLED ]]; then
+  echo "Pull Image Scan target tag. Image Build not enabled, Image Scan enabled."
   docker pull "$WFE_IMAGE_TAG"
 fi
 
