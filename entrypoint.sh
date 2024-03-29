c90e7681f3ecbeeadb79431b408add31817a6494
Bacchus Jackson <34331871+BacchusJackson@users.noreply.github.com>
2024-03-29 13:50:21 -0500

    fix: entrypoint bash syntax

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,16 +7,18 @@ ls -lah
 pwd
 
 if [ -z "$DOCKER_AUTH_JSON" ]; then
-  echo "Skip docker config.json creation, DOCKER_AUTH_JSON not set"
+  shout log "Skip docker config.json creation, DOCKER_AUTH_JSON not set"
 else
-  echo "DOCKER_AUTH_JSON set, creating ~/.docker/config.json"
+  shout log "DOCKER_AUTH_JSON set, creating ~/.docker/config.json"
   mkdir -p ~/.docker
   echo $DOCKER_AUTH_JSON | jq . > ~/.docker/config.json
 fi
 
-if [[ -z "$WFE_IMAGE_BUILD_ENABLED" && -n "$WFE_IMAGE_SCAN_ENABLED" ]]; then
-  echo "Pull Image Scan target tag. Image Build not enabled, Image Scan enabled."
-  docker pull "$WFE_IMAGE_TAG"
+if ([ "$WFE_IMAGE_BUILD_ENABLED" = "0" ] || [ "$WFE_IMAGE_BUILD_ENABLED" = "false" ]); then
+  if ([ "$WFE_IMAGE_SCAN_ENABLED" = "1" ] || [ "$WFE_IMAGE_SCAN_ENABLED" = "true" ]); then
+    shout log "Pull Image Scan target tag. Image Build not enabled, Image Scan enabled."
+    docker pull "$WFE_IMAGE_TAG"
+  fi
 fi
 
 git config --global --add safe.directory $GITHUB_WORKSPACE
