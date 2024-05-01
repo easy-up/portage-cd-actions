69360a850699a94b4ed65e63618a5a211f53cead
Paul Wheeler <pwheeler@revacomm.com>
2024-05-01 06:55:35 -1000

    fix: podman entrypoint had malformed su commands

--- image-build-scan-publish/podman/entrypoint.sh
+++ image-build-scan-publish/podman/entrypoint.sh
@@ -6,14 +6,14 @@ set -e
 chgrp -R podman "$GITHUB_WORKSPACE"
 chmod -R g+w "$GITHUB_WORKSPACE"
 
-su podman -s sh -c "git config --global --add safe.directory $GITHUB_WORKSPACE"
+su podman -s /bin/sh -c "git config --global --add safe.directory $GITHUB_WORKSPACE"
 
 # Configure rootless container storage.
 sed -i -e "s|^#\\? *rootless_storage_path *=.*$|rootless_storage_path=\"$GITHUB_WORKSPACE/.containers/storage\"|" /etc/containers/storage.conf
 
 if [ "$CONTAINER_REGISTRY" != "" ] && [ "$REGISTRY_USER" != "" ] && [ "$REGISTRY_TOKEN" != "" ]; then
 	echo "Logging in to registry $CONTAINER_REGISTRY as $REGISTRY_USER"
-	su podman -s sh -c "echo \"$REGISTRY_TOKEN\" | podman login \"$CONTAINER_REGISTRY\" -u \"$REGISTRY_USER\" --password-stdin"
+	su podman -s /bin/sh -c "echo \"$REGISTRY_TOKEN\" | podman login \"$CONTAINER_REGISTRY\" -u \"$REGISTRY_USER\" --password-stdin"
 fi
 
-su podman -s sh -c "workflow-engine run all --verbose --semgrep-experimental --cli-interface podman"
+su podman -s /bin/sh -c "workflow-engine run all --verbose --semgrep-experimental --cli-interface podman"
