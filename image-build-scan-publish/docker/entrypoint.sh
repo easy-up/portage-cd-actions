11500a7feb872cfe5df32caca33155a4b98fddc8
Paul Wheeler <paul@free-side.net>
2025-02-02 11:47:20 -1000

    Debugging docker image pull issues.

--- image-build-scan-publish/docker/entrypoint.sh
+++ image-build-scan-publish/docker/entrypoint.sh
@@ -14,12 +14,19 @@ else
   shout log "Skip docker config.json creation, DOCKER_AUTH_JSON not set"
 fi
 
+shout log "PORTAGE_IMAGE_BUILD_ENABLED: $PORTAGE_IMAGE_BUILD_ENABLED; PORTAGE_IMAGE_SCAN_ENABLED: $PORTAGE_IMAGE_SCAN_ENABLED"
 if ([ "$PORTAGE_IMAGE_BUILD_ENABLED" = "0" ] || [ "$PORTAGE_IMAGE_BUILD_ENABLED" = "false" ]); then
   if ([ "$PORTAGE_IMAGE_SCAN_ENABLED" = "1" ] || [ "$PORTAGE_IMAGE_SCAN_ENABLED" = "true" ]); then
     shout log "Pull Image Scan target tag. Image Build not enabled, Image Scan enabled."
     docker pull "$PORTAGE_IMAGE_TAG"
+  else
+    shout log "Not pulling image because image scan is disabled"
   fi
+else
+  shout log "Not pulling image because image build is enabled"
 fi
+
+docker image ls
 
 git config --global --add safe.directory $GITHUB_WORKSPACE
 
