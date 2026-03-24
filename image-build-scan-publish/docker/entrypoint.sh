5f49475e669ad656b1d1d4a54ea82614a866c1d7
Paul Wheeler <paul.wheeler@intellibridge.us>
2026-03-23 17:36:11 -1000

    Drop the use of the shout command in entrypoint.sh

--- image-build-scan-publish/docker/entrypoint.sh
+++ image-build-scan-publish/docker/entrypoint.sh
@@ -4,25 +4,25 @@
 set -e
 
 if [ -f "$DOCKER_AUTH_JSON" ]; then
-  shout log "DOCKER_AUTH_JSON set, creating ~/.docker/config.json"
+  echo "DOCKER_AUTH_JSON set, creating ~/.docker/config.json"
   mkdir -p ~/.docker
   echo $DOCKER_AUTH_JSON | jq . > ~/.docker/config.json
 elif [ "$CONTAINER_REGISTRY" != "" ] && [ "$REGISTRY_USER" != "" ] && [ "$REGISTRY_TOKEN" != "" ]; then
-	shout log "Logging in to registry $CONTAINER_REGISTRY as $REGISTRY_USER"
+	echo "Logging in to registry $CONTAINER_REGISTRY as $REGISTRY_USER"
 	echo "$REGISTRY_TOKEN" | docker login "$CONTAINER_REGISTRY" -u "$REGISTRY_USER" --password-stdin
 else
-  shout log "Skip docker config.json creation, DOCKER_AUTH_JSON not set"
+  echo "Skip docker config.json creation, DOCKER_AUTH_JSON not set"
 fi
 
 if ([ "$PORTAGE_IMAGE_BUILD_ENABLED" = "0" ] || [ "$PORTAGE_IMAGE_BUILD_ENABLED" = "false" ]); then
   if ([ "$PORTAGE_IMAGE_SCAN_ENABLED" = "1" ] || [ "$PORTAGE_IMAGE_SCAN_ENABLED" = "true" ]); then
-    shout log "Image Build not enabled, Image Scan enabled. Pulling Image Scan target tag."
+    echo "Image Build not enabled, Image Scan enabled. Pulling Image Scan target tag."
     docker pull "$PORTAGE_IMAGE_TAG"
   fi
 fi
 
 if ([ "$PORTAGE_IMAGE_SCAN_ENABLED" = "1" ] || [ "$PORTAGE_IMAGE_SCAN_ENABLED" = "true" ]); then
-  shout log "Image Scan enabled. Updating grype db."
+  echo "Image Scan enabled. Updating grype db."
   grype db update
 fi
 
--- image-build-scan-publish/podman/entrypoint.sh
+++ image-build-scan-publish/podman/entrypoint.sh
@@ -12,25 +12,25 @@ su podman -s /bin/sh -c "git config --global --add safe.directory $GITHUB_WORKSP
 sed -i -e "s|^#\\? *rootless_storage_path *=.*$|rootless_storage_path=\"$GITHUB_WORKSPACE/.containers/storage\"|" /etc/containers/storage.conf
 
 if [ -f "$DOCKER_AUTH_JSON" ]; then
-  shout log "DOCKER_AUTH_JSON set, creating ~/.docker/config.json"
+  echo "DOCKER_AUTH_JSON set, creating ~/.docker/config.json"
   mkdir -p ~/.docker
   echo $DOCKER_AUTH_JSON | jq . > ~/.docker/config.json
 elif [ "$CONTAINER_REGISTRY" != "" ] && [ "$REGISTRY_USER" != "" ] && [ "$REGISTRY_TOKEN" != "" ]; then
-	shout log "Logging in to registry $CONTAINER_REGISTRY as $REGISTRY_USER"
+	echo "Logging in to registry $CONTAINER_REGISTRY as $REGISTRY_USER"
 	su podman -s /bin/sh -c "echo \"$REGISTRY_TOKEN\" | podman login --compat-auth-file \"\$HOME/.docker/config.json\" \"$CONTAINER_REGISTRY\" -u \"$REGISTRY_USER\" --password-stdin"
 else
-  shout log "Skipping docker config.json creation, neither DOCKER_AUTH_JSON or CONTAINER_REGISTRY/REGISTRY_USER/REGISTRY_TOKEN are set"
+  echo "Skipping docker config.json creation, neither DOCKER_AUTH_JSON or CONTAINER_REGISTRY/REGISTRY_USER/REGISTRY_TOKEN are set"
 fi
 
 if ([ "$PORTAGE_IMAGE_BUILD_ENABLED" = "0" ] || [ "$PORTAGE_IMAGE_BUILD_ENABLED" = "false" ]); then
   if ([ "$PORTAGE_IMAGE_SCAN_ENABLED" = "1" ] || [ "$PORTAGE_IMAGE_SCAN_ENABLED" = "true" ]); then
-    shout log "Image Build not enabled, Image Scan enabled. Pulling Image Scan target tag."
+    echo "Image Build not enabled, Image Scan enabled. Pulling Image Scan target tag."
     su podman -s /bin/sh -c "docker pull \"$PORTAGE_IMAGE_TAG\""
   fi
 fi
 
 if ([ "$PORTAGE_IMAGE_SCAN_ENABLED" = "1" ] || [ "$PORTAGE_IMAGE_SCAN_ENABLED" = "true" ]); then
-  shout log "Image Scan enabled. Updating grype db."
+  echo "Image Scan enabled. Updating grype db."
   GRYPE_DB_CACHE_DIR="$GITHUB_WORKSPACE/.grype-db" su podman -s /bin/sh -c "grype db update"
 fi
 
