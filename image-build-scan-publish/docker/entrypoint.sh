1e209cf72a27e5b2303fcbade8add49e519cd4ae
Paul Wheeler <paul.wheeler@intellibridge.us>
2026-03-26 21:37:57 -1000

    Arrrrg, grep returns error when it finds no results, so dumb

--- image-build-scan-publish/docker/entrypoint.sh
+++ image-build-scan-publish/docker/entrypoint.sh
@@ -30,7 +30,7 @@ git config --global --add safe.directory $GITHUB_WORKSPACE
 
 # In order for --cache-from and --cache-to to work with BuildKit, we need to use the docker-container driver.
 BUILDER_NAME=portage-buildkit-container
-BUILDER_INSTANCE="$(docker buildx ls --format json | jq -r '.Name' | grep $BUILDER_NAME)"
+BUILDER_INSTANCE="$(docker buildx ls --format json | jq -r '.Name' | grep $BUILDER_NAME || echo "")"
 if [ -z "$BUILDER_INSTANCE" ]; then
   echo "[DEBUG] (portage-cd-action): Creating buildx builder instance $BUILDER_NAME"
   docker buildx create --name $BUILDER_NAME --driver docker-container --driver-opt default-load=true --use --bootstrap
